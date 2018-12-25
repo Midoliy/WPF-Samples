60f7f7c1dd5aa9fe5dce17636f37587d4f359e38
midoliy <s85f079p@gmail.com>
2018-12-26 06:36:08 +0900

    [ new ] : ICommand-sample

---
+++ ICommand-sample/ICommand-sample/Model/Property/NotifiableProperty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommand_sample.Model.Property
+{
+    public class NotifiableProperty<T> : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly PropertyChangedEventArgs _arg;
+        private readonly Predicate<T> _predicate;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (value != null && !_value.Equals(value) && _predicate(_value))
+                {
+                    _value = value;
+                    PropertyChanged?.Invoke(this, _arg);
+                }
+            }
+        }
+
+        private T _value;
+
+        public NotifiableProperty(T value = default)
+        {
+            _arg = new PropertyChangedEventArgs(nameof(Value));
+            _predicate = v => true;
+            _value = value;
+        }
+
+        public NotifiableProperty(T value, Predicate<T> predicate)
+            : this(value)
+        {
+            _predicate = predicate;
+        }
+    }
+}
---
+++ ICommand-sample/ICommand-sample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ICommand_sample.Model.Command;
+using ICommand_sample.Model.Property;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommand_sample.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public NotifiableProperty<int> Number { get; set; }
+        public DelegateCommand Command { get; set; }
+        public DelegateCommand<dynamic> Command2 { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Number = new NotifiableProperty<int>(0);
+            Command  = new DelegateCommand(v => Number.Value++, v => Number.Value < 10);
+            Command2 = new DelegateCommand<dynamic>(v => Number.Value++, v => Number.Value < 10);
+        }
+    }
+}
