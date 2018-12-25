9582fe06e0187b400a6138ca0150d4cc9f8af227
midoliy <s85f079p@gmail.com>
2018-12-26 06:35:43 +0900

    [ new ] : INotifyPropertyChanged-sample

---
+++ INotifyPropertyChanged-sample/INotifyPropertyChanged-sample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using INotifyPropertyChanged_sample.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChanged_sample.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public NotifiableProperty<int> Number { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Number = new NotifiableProperty<int>(10);
+        }
+
+    }
+}
