5c679bd686d55da5b2f709d69546b05cf9fcb87f
midoliy <s85f079p@gmail.com>
2019-01-08 06:47:52 +0900

    [ new ] : DataTrigger-sample

---
+++ DataTrigger-sample/DataTrigger-sample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTrigger_sample.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public bool IsViewModelEnabled { get; set; }
+    }
+}
